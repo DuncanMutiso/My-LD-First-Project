7579b943fd79619564ad09d38ec1a29ae075a5d4
Duncan Mutiso <dannykioko02@gmail.com>
2025-09-03 14:13:47 +0300

    Progress

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
-    return HttpResponse('Hello World')
-def new(request):
-    return HttpResponse('New')
+    #return HttpResponse("Thank you Jesus")
+    products = Product.objects.all
+    return render (request, 'index.html', {'products':products})
